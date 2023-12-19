5cb5617c4cc657b5e9d34504c8a435821a0f6a2f
Ayman Velani <secondayman@gmail.com>
2023-12-19 18:17:42 +0530

    comitted changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Getting references to HTML elements
 const input = document.getElementById('placeholder-text');
-const randomMeal = document.getElementsByClassName('random-box');
+const randomMeal = document.getElementById('random-box'); 
 const Meal = document.getElementById('meal');
 const searchbtn = document.getElementById('search');
-const category = document.getElementsByClassName('category');
+const categories = document.getElementsByClassName('category');
 
 // Function to fetch and display a random meal
 async function getRandomMeal() {
@@ -11,14 +11,12 @@ async function getRandomMeal() {
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
         const data = await response.json();
         const meal = data.meals[0];
-        console.log(meal);
 
-        // Displaying random meal details in the designated HTML element
-        const mealdetails = document.getElementById("random-box");
-        mealdetails.innerHTML = `
-        <h4 class="category">${meal.strCategory}</h4>
-        <img src="${meal.strMealThumb}" id="image"  width=150vw />
-        <h3 class="name">${meal.strMeal}</h3>   
+        // Displaying random meal details 
+        randomMeal.innerHTML = `
+           
+            <img src="${meal.strMealThumb}" id="image" width="150vw" />
+            <h3 class="name">${meal.strMeal}</h3>   
         `;
     } catch (error) {
         console.error("Error in fetching random meal data:", error);
@@ -33,7 +31,7 @@ function getMeals() {
         .then((data) => displayMeals(data));
 }
 
-// Function to display meals in the designated HTML element
+// Function to display meals
 function displayMeals(data) {
     const meals = data.meals;
 
@@ -41,18 +39,15 @@ function displayMeals(data) {
     Meal.innerHTML = '';
 
     meals.forEach((el) => {
-        // Displaying category information for each meal
-        category.textContent = el.strCategory;
-        
         // Creating a new tile for each meal
         let newTile = document.createElement('div');
         newTile.setAttribute('class', 'tile');
-        
+
         // Creating an image element for the meal 
         let img = document.createElement('img');
         img.setAttribute('src', el.strMealThumb);
         img.setAttribute('width', '150vw');
-        img.setAttribute('id','image1');
+        img.setAttribute('id', 'image1');
 
         // Creating a name element for the meal
         let name = document.createElement('h3');
@@ -64,10 +59,16 @@ function displayMeals(data) {
         // Appending the tile to the designated HTML element
         Meal.append(newTile);
     });
+
+    // Displaying category information for the first meal
+    if (meals.length > 0) {
+        categories[0].textContent = meals[0].strCategory;
+    }
+
     console.log(data);
 }
 
-// Event listener for the Enter key to trigger the search
+// Event listener for the Enter key to perform search function
 input.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         e.preventDefault();
